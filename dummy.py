11f2bc05a4278b7ea7122dd753bbaba8e2a55003
Padam Gupta <padamgupta1999@gmail.com>
2020-08-31 01:01:07 +0530

    change

--- dummy.py
+++ dummy.py
@@ -10,38 +10,25 @@ from flask import Flask, jsonify, request, render_template
 from torch2vec.torch2vec import LoadModel
 
 app = Flask(__name__)
-model = LoadModel('/home/deviantpadam/Downloads/weights.npy')
+model = LoadModel('/home/deviantpadam/Downloads/weights.npy',f_size=3,pad=[4,2,1])
 
 @app.route('/')
 def home():
-    doc_ids = model.embeddings[:,0].astype('int')
+    doc_ids = model.docids
     if not request.args.get('num'):
         num = 0
     else: 
         num = int(request.args.get('num'))
-        
     return render_template('home.html', doc_ids = doc_ids[25*num:25*(num+1)], num=num)
 
 @app.route('/torch')
 def torch():
     id = request.args.get('id')
-    sim = model.similar_docs(int(id),topk=10,use='torch')
+    sim = model.similar_docs(int(id),topk=10)
     
     similar = dict(zip([int(i) for i in sim[0]],sim[1]))#working
     return jsonify(similar)
 
-@app.route('/sklearn')
-def sklearn():
-    id = request.args.get('id')
-    sim = model.similar_docs(int(id),topk=10,use='sklearn')
-    similar = dict(zip([int(i) for i in sim[0]],sim[1]))#working
-    return jsonify(similar)
-
-
-@app.route('/select')
-def select():
-    ids = request.args.get('id')
-    return render_template('select.html',ids=ids)
 
 if "__main__"==__name__:
     app.run()
